3ac4b0ae9c5579f15e190b5f25674468cc58415f
Ashley Young <aiyoung@iastate.edu>
2017-02-17 15:33:05 -0600

    Just passing through, don't mind me

--- sketch.js
+++ sketch.js
@@ -1,109 +1,164 @@
-// var x = 0;
-
-// function setup() {
-// 	line(15, 25, 70, 90);
-// }
-
-// function draw() {
-
-// }
-var tmap, s, stagIndex;
-var x = 0, y = 0;
-var showCoords = false;
-
+var img;
 function preload() {
-  tmap = loadTiledMap("desert", "data");
+	img = loadImage('water.png')
 }
 
 function setup() {
-  createCanvas(800, 600);
-  ellipseMode(CENTER);
-  s = tmap.getMapSize();
-  fill(0);
+	var myCanvas = createCanvas(600,400);
+	myCanvas.parent('myContainer');
+	line(15, 25, 70, 90);
+	img(img, 0, 0);
+
+	drawingContext.shadowOffsetX = 5;
+    drawingContext.shadowOffsetY = -5;
+    drawingContext.shadowBlur = 10;
+    drawingContext.shadowColor = "black";
+    background(200);
+    ellipse(width/2, height/2, 50, 50);
 }
+/*
+	 Parent tile type -- contains common properties of tiles 
+	class generic_tile {
+		//this.y_pos = y_pos;
+		//this.x_pos = x_pos;
+		constructor(type, src_img, hardness) {
+			this.type = type;
+			this.src_img = src_img;
+			this.hardness = hardness;
+			occupied = false;
+		}
 
-function draw() {
-  background(tmap.getBackgroundColor());
-  tmap.draw(x, y);
-  if(showCoords) {
-    textSize(8);
-    for (var nx = 0; nx < s.x; nx++)
-    for (var ny = 0; ny < s.y; ny++){
-      var p = tmap.mapToCam(createVector(nx, ny));
-      ellipse(p.x, p.y, 4, 4);
-      text(nx + "," + ny, p.x, p.y);
-    }
-  }
-  textSize(24);
-  text("ADWS to move, Z to toogle Map Coordinates, C to change map", 10, 50);
-  text("Corner: Canvas coordinates: " + x + ", " + y, 10, 100);
-  text("Corner: Map coordinates: " + tmap.canvasToMap(x, y).x + ", " + tmap.canvasToMap(x, y).y, 10, 150);
-  text("Map Name: " + tmap.getName(), 10, 200);
-  text("Map Orientation: " + tmap.getOrientation(), 10, 250);
-  if(tmap.getOrientation() == "staggered" || tmap.getOrientation() == "hexagonal") {
-    stagIndex = tmap.staggerindex == 0 ? "odd" : "even";
-    text("Stagger Info: " + tmap.staggeraxis + "," + stagIndex, 410, 250);
-  }
-  text("Map Size: " + tmap.getMapSize(), 10, 300);
-  text("Layer 0 Type: " + tmap.getType(0), 10, 350);
+		is_occupied() {
+			return occupied==true;
+		}
+
+		set_occupied(value) {
+			occupied = value;
+		}
+	}
+
+	class grass_tile extends generic_tile {
+		constructor() {
+			super("grass", "grass.png", 0);
+		}
+	}
+	class sand_tile extends generic_tile {
+		constructor() {
+			super("sand", "sand.png", 10);
+		}
+	}
+	class water_tile extends generic_tile {
+		constructor() {
+			super("water", "water.png", Infinity);
+		}
+	}
+	class forest_tile extends generic_tile {
+		constructor() {
+			super("forest", "forest.png", 25);
+		}
+	}
+	class mountain_tile extends generic_tile {
+		constructor() {
+			super("mountain", "mountain.png", Infinity);
+		}
+	}
+
+	var lines; 
+
+	var tiles = [[]];
+
+	function tile(x, y, width, height, src)
+	{
+		this.x =x;
+		this.y =y;
+		this.width = width;
+		this.height = height;
+		this.src = src;
+
+		this.image = new Image();
+
+		this.update = function()
+		{
+			ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+		}
+	}
+
+	function start()
+	{
+		myGameArea.start();
+		draw_map();
+	}
+
+	var myGameArea = {
+
+		canvas: document.createElement("canvas"),
+
+		start: function(){
+			this.canvas.width = 150;
+			this.canvas.height = 150;
+			this.context = this.canvas.getContext("2d");
+			this.context.imageSmoothingEnabled = false;
+			document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+		}
+	}
+
+	document.getElementById('filechoice').onchange = function(){
+
+			var file = this.files[0];
+
+			var reader = new FileReader();
+
+			lines = reader.readAsText(file);
+
+		reader.onload = function(progressEvent){
+			// Entire file
+
+	    	// By lines
+	    	lines = this.result.split('\n');
+	    	console.log(lines);
+	    	console.log(Array.isArray(lines));
+
+	    	for(line = 0; line < lines.length; line++){
+	      		lines[line] = lines[line].split(""); 
+	      		//document.getElementById('demo')
+	    	}
 
 
+		};
+		console.log(lines)
 
-  if (keyIsPressed) {
-    if(key == 'a' || key == 'A') x -= tmap.getTileSize().x / 2;
-    if(key == 'd' || key == 'D') x += tmap.getTileSize().x / 2;
-    if(key == 'w' || key == 'W') y -= tmap.getTileSize().y / 2;
-    if(key == 's' || key == 'S') y += tmap.getTileSize().y / 2;
-  }
-}
+		start();
 
-function keyPressed(){
-  if(key == 'z' || key == 'Z') showCoords = !showCoords;
-  if(key == 'c' || key == 'C') changeMap();
-}
+	};
 
-function changeMap(){
-  x = 0;
-  y = 0;
-  switch(tmap.getName()){
-    case "desert":
-      // tmap = loadTiledMap("isometric_grass_and_water", "data"); might work here
-      // but callback functions are soooo cool!
-      loadTiledMap("isometric", "data", mapLoaded);
-      fill(255);
-      break;
-    case "isometric":
-      loadTiledMap("staggered_x_even", "data", mapLoaded);
-      fill(255);
-      break;
-    case "staggered_x_even":
-      loadTiledMap("staggered_x_odd", "data", mapLoaded);
-      fill(255);
-      break;
-    case "staggered_x_odd":
-      loadTiledMap("staggered_y_even", "data", mapLoaded);
-      fill(255);
-      break;
-    case "staggered_y_even":
-      loadTiledMap("staggered_y_odd", "data", mapLoaded);
-      fill(255);
-      break;
-    case "staggered_y_odd":
-      loadTiledMap("hexagonal", "data", mapLoaded);
-      fill(0);
-      break;
-    case "hexagonal":
-      loadTiledMap("perspective_walls", "data", mapLoaded);
-      fill(0);
-      break;
-    case "perspective_walls":
-      loadTiledMap("desert", "data", mapLoaded);
-      fill(0);
-      break;
-  }
-}
+	function draw_map() {
+		for (y=0; y< lines.length; y++) {
+			for (x=0; x<lines[0].length; x++) {
+				switch(lines[y][x])
+				{
+					case 'G':
+						tiles.push(new tile(x*30, y*30, 30, 30, "grass.png"));
+						break;
+					case 'D':
+						tiles.push(new tile(x*30, y*30, 30, 30, "dirt.png"));
+						break;
+					case 'W':
+						tiles.push(new tile(x*30, y*30, 30, 30, "water.png"));
+						break;
+				}
+				console.log(lines[y][x]);
+			}
+		}
+	}
 
-function mapLoaded(newMap) {
-  tmap = newMap;
-  s = tmap.getMapSize();
-}
+	function updateGameArea()
+	{
+		setTimeout(function(){
+				requestAnimationFrame(updateGameArea)
+		}, 1000/ 12);
+
+		for(i = 0; i < tile.length; i++)
+		{
+			tiles[i].update();
+		}
+	}*/
