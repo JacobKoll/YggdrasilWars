3617c625a776b0c71cd09ebf5eb357c21edbb465
Chan Davis <cfdavis@iastate.edu>
2017-02-21 17:57:38 -0600

    Website with examples and creation of demo game.
    - index.html now has buttons that lead to the 3 different examples that
    we have
    - Began work on the game for the demo.
    - created Fighter.js, multifighter.js, multifighter.html
    - Moved sprites into folders for each sprite and subfolders for each of
    those sprite's animations

--- public/SpaceBoy.js
+++ public/SpaceBoy.js
@@ -2,6 +2,7 @@ var spaceboy;
 var spaceboys = [];
 var spritesheet;
 var flail_animation;
+var love_animation;
 
 var explode_sprite_sheet;
 var explode_sprite;
@@ -11,25 +12,36 @@ var socket;
 // Load the images and animations before the game starts.
 function preload()
 {
-  flail_animation = loadAnimation('assets/spaceboy0000.png', 'assets/spaceboy0008.png');
-
+  flail_animation = loadAnimation('assets/spaceboy/flail/spaceboy0000.png', 'assets/spaceboy/flail/spaceboy0008.png');
+  love_animation = loadAnimation('assets/spaceboy/love/spacelove0000.png', 'assets/spaceboy/love/spacelove0008.png');
 }
 
 function setup()
 {
-  createCanvas(500, 500);
+  var canvas = createCanvas(500, 500);
+  canvas.parent('canvas_pos');
   socket = io.connect('http://localhost:3000');
-
   spaceboy = createSprite(200, 165, 64, 64);
   spaceboy.addAnimation('flail', flail_animation);
+  spaceboy.addAnimation('love', love_animation);
   spaceboy.friction =.95;
   spaceboy.maxSpeed = 6;
 
-//  socket.emit('start', data);
-//  socket.on('heartbeat', function(data) {
-//    spaceboys = data;
-//  });
-  console.log(getSprites());
+  // Initialize the data
+  var data = {
+    x: spaceboy.position.x,
+    y: spaceboy.position.y,
+    rotation: spaceboy.rotation
+  };
+
+
+  socket.emit('start', data);
+  socket.on('heartbeat', function(data) {
+    spaceboys = data;
+  });
+
+
+  console.log(spaceboys);
 
 
 }
@@ -38,6 +50,14 @@ function draw()
 { 
   clear();
   background(51);
+
+  // for(var i = 0; i < spaceboys.length; i++)
+  // {
+  //   push();
+  //   translate(spaceboys[i].x, spaceboys[i].y)
+  //   rotate(spaceboys[i].rotation);
+  //   pop();
+  // }
 
   /* Controls go below */
   if(keyDown("d"))
@@ -52,6 +72,16 @@ function draw()
   {
     spaceboy.addSpeed(.2, spaceboy.rotation);
   }
+  if(keyDown(" "))
+  {
+    spaceboy.changeAnimation('love');
+  }
+  else
+  {
+    spaceboy.changeAnimation('flail');
+  }
+
+
 
   if(spaceboy.position.x > width + 20)
   {
@@ -69,6 +99,15 @@ function draw()
   {
     spaceboy.position.y = height + 20;
   }
+
+  // Update the data
+  var data = {
+    x: spaceboy.position.x,
+    y: spaceboy.position.y,
+    rotation: spaceboy.rotation
+  };
+
+  socket.emit('update', data);
 
   document.getElementById("x").innerHTML = Number(Math.round(spaceboy.position.x + 'e1')+'e-1');
   document.getElementById("y").innerHTML = Number(Math.round(spaceboy.position.y + 'e1')+'e-1');
