ee23f03d2f62400b2eeb860fae0e0e39ed1717fc
Andrew <amesser@iastate.edu>
2017-03-24 12:54:41 -0500

    Mini-map addition

---
+++ public/fighter-map.js
@@ -0,0 +1,95 @@
+// This is the Fighter class that allows for multiple instances of a "Fighter" sprite with custom properties
+
+// These values are defined here for ease of access
+var turnSpeed = 1.57;
+var acceleration = 1.753;
+var maxSpeed = 2;
+var friction = .5;
+
+function Fighter(health, x, y, walkAnimation, swingAnimation, deathAnimation, idleAnimation)
+{
+	/* Properties of this fighter */
+	this.health = health; //Amount of health.
+
+	this.alive = true;
+
+	/* Initialize the animations */
+	this.walkAnimation = walkAnimation;// Walk animations
+	this.swingAnimation = swingAnimation; // Attack animation
+	this.deathAnimation = deathAnimation; // Death animation
+	this.idleAnimation = idleAnimation; // Animation for when the player is not moving
+
+	/* This is where we initialize the sprite and it's animations */
+	this.sprite = createSprite(x, y, 72, 96);
+	this.sprite.rotateToDirection = true;
+	this.sprite.maxSpeed = maxSpeed;
+	this.sprite.friction = friction;
+	this.sprite.debug = true;
+
+
+	this.sprite.addAnimation('walk', walkAnimation);
+	this.sprite.addAnimation('death', deathAnimation);
+	this.sprite.addAnimation('idle', idleAnimation);
+
+	this.sword = createSprite(x, y, 138, 96);
+	this.sword.rotateToDirection = true;
+	this.sword.maxSpeed = maxSpeed;
+	this.sword.friction = friction;
+	this.sword.debug = true;
+	this.sword.visible = false;
+
+	this.sword.addAnimation('swing', swingAnimation);
+
+	this.sword.position = this.sprite.position;
+
+	/* Bounding boxes */
+	this.sprite.setCollider("circle", 0, 0, 45);
+
+	// Move forward
+	this.walk = function(direction)
+	{
+		if(this.alive && this.sword.visible == false)
+		{
+			if( Math.round(this.sprite.position.x) < Math.round(camera.mouseX) + 4 &&
+				Math.round(this.sprite.position.x) > Math.round(camera.mouseX) - 4 &&
+			 	Math.round(this.sprite.position.y) < Math.round(camera.mouseY) + 4 &&
+				Math.round(this.sprite.position.y) > Math.round(camera.mouseY) - 4)
+			{
+				this.sprite.changeAnimation('idle');
+			}
+			else
+			{
+				this.sprite.attractionPoint(100, camera.mouseX, camera.mouseY);
+				this.sword.attractionPoint(50, camera.mouseX, camera.mouseY);
+				this.sprite.addSpeed(acceleration, this.sprite.getDirection());
+				this.sprite.changeAnimation('walk');
+
+				/* Trying to make the bounding box for the sword follow the rotation */
+				this.sword.setCollider("circle",
+					60 * cos(radians(this.sprite.getDirection() - 16)),
+					60 * sin(radians(this.sprite.getDirection() - 16)),
+					46);
+			}
+		}
+	}
+
+	this.swing = function()
+	{
+		this.sword.visible = true;
+		this.sprite.setSpeed(0);
+	}
+
+	this.die = function()
+	{
+		this.alive = false;
+		this.sprite.remove();
+		this.sword.remove();
+	}
+
+	this.remove = function()
+	{
+		this.sprite.remove();
+		this.sword.remove();
+	}
+
+}
---
+++ server/YggdrasillServer.js
@@ -0,0 +1,114 @@
+
+var mysql = require('mysql');
+var express = require("express");
+var socket = require("socket.io");
+var app = express();
+var server = app.listen(3000);
+var io = socket(server);
+
+var FPS = 24;
+
+var fighterArr = [];
+var spawnerArr = [];
+var enemyArr = [];
+var obstacleArr = [];
+var chestArr = [];
+
+app.use(express.static("../public"));
+
+var chestArr;
+/**
+ * Initializes everything that the server will need.
+ */
+function init()
+{
+	fighterArr = [];
+
+	spawnerArr = [];
+	enemyArr = [];
+
+	/**
+	 * Initialize obstacles
+	 */
+	for (var i=0; i<20; i++) {
+		var a = Math.floor((Math.random())*2000/45)*(45);
+		var b = Math.floor((Math.random())*1450/45)*(45);
+		var obsData = {x: a, y: b};
+		obstacleArr.push(obsData);
+	}
+	console.log("Obstacles Generated");
+
+	/**
+	 * Initialize chests
+	 */
+	 for (var i=0; i<4; i++) {
+		var a = Math.floor((Math.random())*2000/45)*(45);
+		var b = Math.floor((Math.random())*1450/45)*(45);
+		var chestData = {x: a, y: b};
+		chestArr.push(chestData);
+	}
+	console.log("Chests Generated");
+
+	console.log("The Yygdrasill Wars server is open and running... \n");
+}
+
+/**
+ * This handles the events that occur whenever someone joins the server.
+ * @param  {socket} client [The socket that the client uses to connect to the server]
+ */
+
+
+function onSocketConnect(client)
+{
+
+	setInterval(heartbeat, 1000/FPS);
+
+	/* When connected, add the client's fighter to the array. */
+	client.on('start', function(newFighter)
+	{
+		io.sockets.emit('generateObstacles', obstacleArr);
+		io.sockets.emit('generateChests', chestArr);
+		console.log(client.id + " added it's fighter\n");
+	});
+
+	/**
+	 * Update all of the sprites to be rendered client-side for all clients.
+	 */
+	function heartbeat()
+	{
+		io.sockets.emit('updateFighters' , fighterArr);
+		io.sockets.emit('updateSpawners' , spawnerArr);
+		io.sockets.emit('updateEnemies'  , enemyArr);
+		io.sockets.emit('updateObstacles', obstacleArr);
+		io.sockets.emit('updateChests'   , chestArr);
+
+	}
+
+	console.log(client.id + " has connected to the server.\n");
+
+	client.on('addChest', function(givenX, givenY)
+	{
+		var chestData = {x: givenX, y: givenY};
+		chestArr.push(chestData);
+		io.sockets.emit('updateChests'   , chestArr);
+		console.log(client.id + " added a chest at (" + givenX + ", " + givenY + ")\n");
+	});
+
+	client.on('addObstacle', function(givenX, givenY)
+	{
+		var obstacleData = {x: givenX, y: givenY};
+		obstacleArr.push(obstacleData);
+		io.sockets.emit('updateObstacles', obstacleArr);
+		console.log(client.id + " added an obstacle at (" + givenX + ", " + givenY + ")\n");
+	});
+
+	io.on('disconnect', function()
+	{
+		console.log(client.id + " has disconnected from the server.\n");
+	});
+}
+
+io.on('connection', onSocketConnect);
+
+init();
+
