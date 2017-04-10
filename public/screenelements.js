f80cad7ddefc02315fef819f00710569b8ea5900
Chan Davis <cfdavis@iastate.edu>
2017-04-10 12:49:39 -0500

    Further implementation of screen objects
    - pregamescreens.js is what will be used for the screens that show up
    before the game even starts. This is subject to change if we want a
    different file for each screen.
    - The button object function now allows you to give custom text in
    javascript, versus hard-baked into the button image.
    - Text arguments include the text and the size you want the text
    - Added a formula that should center the text on the button. This for
    simple implementation. For more diverse buttons, you will have to
    hardcode into the image.

--- public/screenelements.js
+++ public/screenelements.js
@@ -2,21 +2,42 @@
  * This file contains various object functions for elements that appear multiple times per screen and can be used on any string.
  */
 
-function Button(x, y, defaultImage, clickedImage, functionToCall)
+function Button(x, y, givenText, textSize, defaultImage, clickedImage, functionToCall)
 {
-	this.sprite = createSprite(y, x);
+	this.x = x;
+	this.y = y;
+
+	this.sprite = createSprite(x, y, 100, 100);
 	this.sprite.addImage('default', defaultImage);
 	this.sprite.addImage('clicked', clickedImage);
+	this.sprite.setDefaultCollider();
+	this.sprite.functionToCall = functionToCall;
+	this.sprite.onMousePressed = this.press;
+	this.sprite.onMouseReleased = this.release;
+	this.buttonText = givenText;
+	this.textSize = textSize;
 
-	this.functionToCall = functionToCall;
-
+	this.sprite.debug = true;
 }
 
 Button.prototype.press = function()
 {
-	this.sprite.changeImage('clicked');
+	this.changeImage('clicked');
 	this.functionToCall();
-	this.sprite.changeImage('default');
 };
+
+Button.prototype.release = function() 
+{
+	this.changeImage('default');
+};
+
+Button.prototype.draw = function() 
+{
+	drawSprite(this.sprite);
+	textAlign(CENTER);
+	textSize(this.textSize);
+	text(this.buttonText, this.x, this.y + (this.textSize / 3));
+};
+
 
 
